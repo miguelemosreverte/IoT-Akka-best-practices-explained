f649b36d2fd8f37f7e7eeef7ecc93e50c9b1d6eb
miguelemosreverte <miguelemosreverte@gmail.com>
2021-10-13 19:19:38 -0300

    Now deserializers can validate strings

--- infrastructure/serialization/src/main/scala/infrastructure/serialization/algebra/Deserializer.scala
+++ infrastructure/serialization/src/main/scala/infrastructure/serialization/algebra/Deserializer.scala
@@ -2,10 +2,20 @@ package infrastructure.serialization.algebra
 
 import infrastructure.serialization.algebra.Deserializer.`failed to deserialize`
 
-trait Deserializer[A] {
+trait Deserializer[A] extends Deserializer.DeserializationValidator {
   def deserialize(in: String): Either[`failed to deserialize`, A]
+  final override def validate: String => Either[`failed to deserialize`, Unit] =
+    deserialize _ andThen simplify
 }
 
 object Deserializer {
   case class `failed to deserialize`(explanation: String)
+  trait DeserializationValidator {
+    def simplify[A]: Either[`failed to deserialize`, A] => Either[`failed to deserialize`, Unit] = {
+      case Left(explanation) => Left(explanation)
+      case Right(value) => Right()
+    }
+
+    def validate: String => Either[`failed to deserialize`, Unit]
+  }
 }
